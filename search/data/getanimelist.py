db10abec3c69e7e5dd460996d0986d1a7a5b9fe0
Fanny Bagus Ramadhan <fannybagus9f@gmail.com>
2023-09-03 20:04:53 +0700

    Update getanimelist.py

--- search/data/getanimelist.py
+++ search/data/getanimelist.py
@@ -10,21 +10,28 @@ start_num = 1
 end_num = 91
 animelist = []
 
-for num in range(start_num, end_num + 1):
-    url = url_template.format(num)
-    print("Getting data for page ", num)
-    response = requests.get(url)
-    data = response.json()
-    animelist.extend(data)
+try:
+    for num in range(start_num, end_num + 1):
+        url = url_template.format(num)
+        print("Getting data for page", num)
+        response = requests.get(url)
 
-with open('anime.json', 'w') as f:
-    json.dump(animelist, f)
+        # Check if the request was successful
+        if response.status_code == 200:
+            data = response.json()
+            animelist.extend(data)
+        else:
+            print(f"Failed to fetch data for page {num}. Status code: {response.status_code}")
 
-with open('anime.json', 'r') as f:
-    count = json.load(f)
+    with open('anime.json', 'w') as f:
+        json.dump(animelist, f)
 
-num_objects = len(count)
+    num_objects = len(animelist)
 
-print("Done! Saved as anime.json")
-print("Total Anime:", num_objects)
-time.sleep(1000)
+    print("Done! Saved as anime.json")
+    print("Total Anime:", num_objects)
+except Exception as e:
+    print("An error occurred:", str(e))
+finally:
+    # Add a sleep to prevent the script from exiting immediately
+    time.sleep(10)  # Sleep for 10 seconds (adjust as needed)
